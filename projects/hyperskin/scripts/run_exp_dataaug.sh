fd100df0667ed03bdc937cd1391c96c748356bee
ana_caznok <a231745@dac.unicamp.br>
2025-12-12 10:48:34 -0300

    not synthetic data yet because no kfold training images yet

--- projects/hyperskin/scripts/run_exp_dataaug.sh
+++ projects/hyperskin/scripts/run_exp_dataaug.sh
@@ -11,10 +11,10 @@ bash scripts/run_kfold.sh configs/model/hsi_classifier_densenet201_best.yaml con
 bash scripts/run_kfold.sh configs/model/densenet201_best_focalloss.yaml configs/data/hsi_dermoscopy_croppedv2_notaug.yaml 5
 
 #no data augmentations k-fold training yes pretrained synthetic data model
-bash scripts/run_kfold.sh configs/model/hsi_classifier_densenet201_best.yaml configs/data/hsi_dermoscopy_croppedv2_notaugsynth.yaml 5
+#bash scripts/run_kfold.sh configs/model/hsi_classifier_densenet201_best.yaml configs/data/hsi_dermoscopy_croppedv2_notaugsynth.yaml 5
 
 #with data augmentations k-fold training yes pretrained synthetic data model
-bash scripts/run_kfold.sh configs/model/hsi_classifier_densenet201_best.yaml configs/data/hsi_dermoscopy_croppedv2_augsynth.yaml 5
+#bash scripts/run_kfold.sh configs/model/hsi_classifier_densenet201_best.yaml configs/data/hsi_dermoscopy_croppedv2_augsynth.yaml 5
 
 
 
